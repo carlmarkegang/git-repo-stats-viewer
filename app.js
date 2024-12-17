dd26d582b3c5476dbaae2c535446d08048dd7293
Carl Markegång <huledule@hotmail.com>
2024-12-17 13:39:51 +0100

    fixes

--- app.js
+++ app.js
@@ -1,23 +1,19 @@
 const fs = require('fs');
 const { exec } = require('child_process');
 const args = process.argv;
-
-
 let repoPath = args[2];
-let data = '<h1>Test</h1><pre>';
-
 if (repoPath == undefined) {
     repoPath = "empty"
 }
-console.log("argument: " + repoPath);
 
-fs.writeFile('index.html', data, (err) => {
-    if (err) {
-        console.error('Error writing to file:', err);
-    } else {
-        console.log('File written successfully!');
-    }
-});
+let dataToWrite = "";
+let userList = [];
+let userListStatus = [];
+let data = `<h1>Test</h1>
+<div>Error reading log</div>`;
+
+
+fs.writeFile('index.html', data, (err) => { });
 
 
 exec("git log --format='%aN' | sort -u", { cwd: '' }, (error, stdout, stderr) => {
@@ -30,37 +26,57 @@ exec("git log --format='%aN' | sort -u", { cwd: '' }, (error, stdout, stderr) =>
         return;
     }
 
-    splitText = stdout.split("\n");
+    splitText = stdout.trim().split("\n");
     for (let i = 0; i < splitText.length; i++) {
-        getInfo(splitText[i].replace("'", "").replace("'", ""));
-      }
-    
+        let username = splitText[i].replace("'", "").replace("'", "");
+        userList.push(username);
+        userListStatus.push(false);
+        getInfo(username, i);
+    }
+
 });
 
 
-function getInfo(name) {
+function getInfo(name, i) {
     exec(`git log --author="${name}" --shortstat --pretty="%an"`, { cwd: '' }, (error, stdout, stderr) => {
-        if (error) {
-            console.error(`Error executing command: ${error.message}`);
-            return;
-        }
-        if (stderr) {
-            console.error(`stderr: ${stderr}`);
-            return;
+        console.log("Reading data for: " + name)
+        userListStatus[i] = true;
+        let formatOutput = stdout.trim().split("\n");
+        dataToWrite += "<div><strong>" + name + "</strong></div>";
+        for (let i = 0; i < formatOutput.length; i++) {
+            if (formatOutput[i] == "" || formatOutput[i] == name) {
+                continue;
+            }
+            dataToWrite += formatOutput[i] + "<br>";
         }
 
-        AppendDataToIndex(stdout)
         return stdout;
     });
 
 }
 
 function AppendDataToIndex(dataToAdd) {
-    fs.appendFile('index.html', dataToAdd, (err) => {
-        if (err) {
-            console.error('Error appending to file:', err);
-        } else {
-            console.log('Data appended to file successfully!');
-        }
+
+    fs.readFile('index.html', function (err, readData) {
+        let DataToAddReplace = readData.toString().replace("Error reading log", dataToAdd)
+
+        fs.writeFile('index.html', DataToAddReplace, (err) => {
+            process.exit();
+        });
     });
+
 }
+
+function validateStatus() {
+    for (let i = 0; i < userListStatus.length; i++) {
+        if (userListStatus[i] == false) {
+            return;
+        }
+    }
+
+    AppendDataToIndex(dataToWrite);
+
+}
+
+const validateStatusInterval = setInterval(validateStatus, 1000);
+
